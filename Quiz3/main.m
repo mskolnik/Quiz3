96274627a38f39d6759be6179f49538d0ebf9819
MariAnne Skolnik <marianneskolnik@MariAnnes-MacBook-Pro.local>
2014-01-31 09:59:42 -0500

    Initial Commit

---
+++ Quiz3/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Quiz3
+//
+//  Created by MariAnne Skolnik on 1/31/14.
+//  Copyright (c) 2014 MariAnne Skolnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Quiz3AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz3AppDelegate class]));
+    }
+}
