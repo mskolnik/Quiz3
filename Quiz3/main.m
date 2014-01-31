5f2838b7599b3ef8130613bd9d614f553fbcb51b
MariAnne Skolnik <marianneskolnik@MariAnnes-MacBook-Pro.local>
2014-01-31 10:43:12 -0500

    second commit

--- Quiz3/main.m
+++ Quiz3/main.m
@@ -13,6 +13,51 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Quiz3AppDelegate class]));
+        NSLog(@"MariAnne");
+        NSString *firstName = @"MariAnne";
+        NSLog(firstName);
+        NSNumber *num = @200;
+        NSLog(num);
+        NSMutableArray *fruits;
+        [fruits addObject:@"apple"];
+        [fruits addObject:@"orange"];
+        [fruits addObject:@"banana"];
+        
+        for(id item in fruits)
+        {
+            NSLog(@"Fruit: %@", item);
+        }
+        
+        NSArray *sweetLevel= [NSArray arrayWithObjects:@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10"];
+        NSDictionary *sweet =[NSDictionary dictionaryWithObjects:fruits forKeys:sweetLevel];
+        
+        NSLog(@"Fruit and their sweetness level: %@ %@", self.sweetLevel, self.fruits);
+        
+        NSUInteger myUInteger = 123;
+        NSLog(myUInteger);
+        
+        NSNumber *one, *two;
+        one = [NSNumber numberWithInt:2];
+        two = [NSNumber numberWithInt:4];
+        NSNumber *final = [one decimalNumberByMultiplyingBy: two];
+        NSLog(final);
+        
+        NSString *lastName = @"Skolnik";
+        
+        NSString *fullName = (@"%@ %@", firstName, lastName);
+        NSLog(fullName);
+        
+        for (id item in fruits){
+            if (sweetLevel < 4) {
+                NSLog(@"Not Sweet");
+            }
+            else if (sweetLevel > 7){
+                NSLog(@"Sweet");
+            }
+            else
+                NSLog(@"Somewhate Sweet");
+        }
+        
+        
     }
 }
